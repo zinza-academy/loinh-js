3529b10735c067f74e5ec79ab20c0fad33a6ce90
loinh <nguyenhuuloi17032004@gmail.com>
2025-07-02 15:35:30 +0700

    fix: add optional decorator in dto

--- backend/lib/shared/dtos/vaccination-site/update-vaccination-site.dto.ts
+++ backend/lib/shared/dtos/vaccination-site/update-vaccination-site.dto.ts
@@ -1,11 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateVaccinationSiteDto {
   @IsString()
+  @IsOptional()
   name: string;
 
+  @IsOptional()
   @IsString()
   headOfVaccination: string;
 
+  @IsOptional()
   wardId: number;
 }
